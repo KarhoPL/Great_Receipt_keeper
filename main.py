bb8734774e28df0d2373c490fc623c6a182f1788
Karolina Haracz <k.m.haracz@gmail.com>
2020-09-15 11:58:24 +0200

    Errors and wrong recognizing handling
    Adding handling errors lines and checking if all values was recognized correctly

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import pytesseract
 import pandas as pd
 import csv
 import numpy as np
-
 import sys
 import codecs
+import datetime as dt
+from math import floor
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\kharacz\AppData\Local\Tesseract-OCR\tesseract.exe'
-#funkcja zwracająca lisę zakupów z zdjecia paragonu
-def products(receipt_image):
+#funkcja zwracająca listę zakupów z zdjecia paragonu
+def Creating_DF_from_image(receipt_image):
     products_list_to_df = []
     im = Image.open(receipt_image) # the second one 
     im = im.filter(ImageFilter.MedianFilter())
     enhancer = ImageEnhance.Contrast(im)
     im = enhancer.enhance(2)
+    im = im.convert('1')
     im.save('unnamed2.jpg')
-    lines =  (pytesseract.image_to_string(Image.open('unnamed2.jpg'))).split("\n")
+    lines =  (pytesseract.image_to_string(Image.open('unnamed2.jpg'),config = '-c tessedit_char_whitelist="01234567890ABCDEFGHIJKLMNOPRSTUVWXYZabcdefghijklmnopqrstuvwxyz-*,. " --psm 6' )).split("\n")
     first_product = 0
     last_product = 0
     for line in lines:
@@ -26,7 +28,7 @@ def products(receipt_image):
     for i in range(first_product,last_product):
         bought_products.append(lines[i])
     for product_line in bought_products:
-        if "*" in product_line:
+        if "*" in product_line and "rabat" not in product_line and "RABAT" not in product_line:
             splited_prod = product_line.split(" ") 
             for word in splited_prod:
                 if "*" in word: star_index = splited_prod.index(word)
@@ -35,54 +37,31 @@ def products(receipt_image):
                 #when star and price got together:
                 if splited_prod[star_index][0] == "*":
                     price_per_unit = float(splited_prod[star_index][1:].replace(",","."))     
-                    amount = float(splited_prod[:-1].replace(",","."))   
+                    amount = float(splited_prod[:-1].replace(",","."))
                     name = " ".join(splited_prod[:star_index - 1])
                 #when star and quantity got together:
                 elif splited_prod[star_index][-1] == "*":
                     price_per_unit = float(splited_prod[star_index+1].replace(",","."))
-                    amount = float(splited_prod[star_index][:-1].replace(",","."))   
+                    amount = float(splited_prod[star_index][:-1].replace(",","."))
                     name = " ".join(splited_prod[:star_index])
             else: 
                 amount = float(splited_prod[star_index - 1].replace(",","."))
+                if amount > 6 and len(str(amount)) > 3 :
+                    amount = (round((amount - 6)*1000))/1000
                 price_per_unit = float(splited_prod[star_index + 1].replace(",","."))
                 name = " ".join(splited_prod[:star_index - 1])
-            final_charge = amount * price_per_unit
-            products_list_to_df.append([name, price_per_unit, amount,final_charge])
+            # handling if one of valueas was wrongly recognized 
+            final_charge_counted = (floor((amount * price_per_unit)*100))/100
+            final_charge = float(splited_prod[-2].replace(",","."))
+            input_to_float = lambda imputing_value, old_value : float(imputing_value.replace(",",".")) if imputing_value else old_value
+            if final_charge != final_charge_counted:
+                print(f'{name} | {price_per_unit} | {amount} | {final_charge} | {final_charge_counted}')
+                print(f"Error in recognition of product, please write correct values for {name}")
+                amount = input_to_float(input("Amount: "), amount)
+                price_per_unit = input_to_float(input("Price per unit of:"), price_per_unit)
+                final_charge = input_to_float(input("Final charge of:"), final_charge)
+            products_list_to_df.append([name, price_per_unit, amount, final_charge])
     df = pd.DataFrame(np.array(products_list_to_df), columns = ["Product Name", "Price", "Amount", "Final Charge"])
     return df
 
-
-produkty = products("unnamed.jpg")
-print(produkty)
-
-#funkcja zwracająca df z listą produktów, ilościa i ceną
-
-def df_products(products):
-    products_list_to_df = []
-    for product_line in products:
-        if "*" in product_line:
-            splited_prod = product_line.split(" ") 
-            for word in splited_prod:
-                if "*" in word: star_index = splited_prod.index(word)
-            # handling errors when the star didn't get separeted from price or quantity
-            if len(splited_prod[star_index])>1:
-                #when star and price got together:
-                if splited_prod[star_index][0] == "*":
-                    price_per_unit = float(splited_prod[star_index][1:].replace(",","."))     
-                    amount = float(splited_prod[:-1].replace(",","."))   
-                    name = " ".join(splited_prod[:star_index - 1])
-                #when star and quantity got together:
-                elif splited_prod[star_index][-1] == "*":
-                    price_per_unit = float(splited_prod[star_index+1].replace(",","."))
-                    amount = float(splited_prod[star_index][:-1].replace(",","."))   
-                    name = " ".join(splited_prod[:star_index])
-            else: 
-                amount = float(splited_prod[star_index - 1].replace(",","."))
-                price_per_unit = float(splited_prod[star_index + 1].replace(",","."))
-                name = " ".join(splited_prod[:star_index - 1])
-            final_charge = amount * price_per_unit
-            products_list_to_df.append([name, price_per_unit, amount,final_charge])
-    df = pd.DataFrame(np.array(products_list_to_df), columns = ["Product Name", "Price", "Amount", "Final Charge"])
-    return df    
-#print(df_products(produkty))
-    
+print(Creating_DF_from_image('unnamed3.jpg'))
